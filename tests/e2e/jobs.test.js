ef92cf9905723e586f543f8048bec6549818bd42
Cosmo Kent <cosmolightfoot@gmail.com>
2019-06-13 17:43:22 -0700

    route tests done

---
+++ tests/e2e/jobs.test.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+
+// const createJob = job => {
+//   return request(app) 
+//     .post('app/v1/jobs')
+//     .send(job)
+//     .then(res => res.body);
+// };
+
+describe('Jobs route', () => {
+  beforeAll(() => {
+    return connect();
+  });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  const newJob = {
+    title: 'CEO',
+    company: 'Microsoft',
+    location: '80014',
+    listing: {
+      jobDescriptionText: 'this is a cool job',
+      jobUrl: 'www.microsoft.com',
+      salary: '$$$10000000000000000000000000'
+    },
+    tracking: 'jobOffer',
+    active: true
+  };
+
+  it('can POST a job', async() => {
+    const job = await request(app)
+      .post('/api/v1/jobs')
+      .send(newJob);
+    expect(job.body).toEqual({ ...newJob, __v: 0, _id: expect.any(String) });
+  });
+});
